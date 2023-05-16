f61ba4e044d49ae8efd7412e79937bceab97cbe5
Niraj Yogesh Wani <106158322+codexniraj@users.noreply.github.com>
2023-05-16 21:55:14 +0530

    Add files via upload

---
+++ Servlet Programming/src/Exp10/servlet.java
@@ -0,0 +1,47 @@
+package Exp10;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+ 
+
+/**
+ * Servlet implementation class servlet
+ */
+@WebServlet("/servlet")
+public class servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html");
+	    String content = "<html><head><title>Niraj's Project</title></head><body><h1>Hi, This is Niraj's Project</h1></body></html>";
+	    response.setContentLength(content.length());
+	    PrintWriter out = response.getWriter();
+	    out.print(content);
+	    out.flush();
+	    out.close();
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
